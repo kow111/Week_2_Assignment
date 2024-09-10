8ff3d4e67cb46effed3a168d53684384276ce5e6
Pham Ngoc Thach <chome710@gmail.com>
2024-09-10 21:05:40 +0700

    minor fix

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,3 @@
-// src/screens/HomeScreen.js
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
